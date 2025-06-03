60b2c728a912433fc5cce43aa79752d8b72059ee
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-06-03 20:58:29 +0300

    fix: add extends abstract entity

--- src/main/java/gr/aueb/cf/teacherApp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/teacherApp/model/Teacher.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Setter
 @Builder
 @Table(name="teachers")
-public class Teacher {
+public class Teacher extends AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
