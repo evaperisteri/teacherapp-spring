548583a1eccc1817e3214a98fda47123f78f277c
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-06-03 20:35:06 +0300

    feat: add insert teacher dto

---
+++ src/main/java/gr/aueb/cf/teacherApp/dto/TeacherInsertDTO.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.teacherApp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class TeacherInsertDTO {
+    @NotNull(message = "Το όνομα δεν μπορεί να είναι κενό")
+    @Size(min=2, message = "Το όνομα πρέπει να έχει πάνω από 2 χαρακτήρες")
+    private String firstname;
+    @NotNull(message = "Το επώνυμο δεν μπορεί να είναι κενό")
+    @Size(min=2, message = "Το επώνυμο πρέπει να έχει πάνω από 2 χαρακτήρες")
+    private String lastname;
+    @Pattern(regexp = "\\d{9,}", message= "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από 9 ψηφία")
+    private String vat;
+    @NotNull(message = "Η περιοχή δεν μπορεί να είναι κενή")
+    private Long regionId;
+
+}
