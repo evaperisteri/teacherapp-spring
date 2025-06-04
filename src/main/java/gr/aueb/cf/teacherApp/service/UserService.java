34c231803329665c70466723813fa5eccbc994d1
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-06-04 18:35:49 +0300

    feat: add user service

---
+++ src/main/java/gr/aueb/cf/teacherApp/service/UserService.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.teacherApp.service;
+
+import gr.aueb.cf.teacherApp.model.User;
+import gr.aueb.cf.teacherApp.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+//    @Autowired
+//    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+
+    @Transactional(rollbackOn = Exception.class)
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword())); //encrypt password
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
